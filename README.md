d12bbc108cb1036d5f602b5021a599644d5999be
Andres Garcia Alves <andres_garcia_ao@hotmail.com>
2019-12-05 17:36:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mini Battle City
+A Battle-City (NES) clon, developed in HTML5/Phaser3.
+
+Developed for subject 'Lenguajes de Programación', on CAECE University, Buenos Aires, Argentina.
+
+This is the first public release of this proyect.
+If you want, please send to me your feedback about the game: andres.garcia.alves@gmail.com
+
+Enjoy!
---
+++ src/app.ts
@@ -0,0 +1,55 @@
+// import "phaser";
+import { RunGame } from "./scenes/rungame";
+
+const defaultConfig: Phaser.Types.Core.GameConfig = {
+  backgroundColor: "#000000",
+  height: 720,
+  parent: "game",
+  physics: {
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+    default: "arcade",
+  },
+  render: { pixelArt: true, antialias: false },
+  scene: [new RunGame()],
+  title: "Mini Battle City",
+  type: Phaser.AUTO,
+  width: 768,
+};
+
+export class BattleCityGame extends Phaser.Game {
+
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+
+}
+
+window.onload = () => {
+  const game = new BattleCityGame(defaultConfig);
+};
+
+/* window.onload = () => {
+  window.focus();
+  const game = new BattleCityGame(config);
+  resizeGame();
+  window.addEventListener("resize", resizeGame);
+}; */
+
+/* function resizeGame() {
+  const canvas: (HTMLCanvasElement | null) = document.querySelector("canvas");
+  if (canvas !== null) {
+      const windowWidth: number = window.innerWidth;
+      const windowHeight: number = window.innerHeight;
+      const windowRatio: number = windowWidth / windowHeight;
+      const gameRatio: number = 750 / 1334;
+      if (windowRatio < gameRatio) {
+          canvas.style.width = windowWidth + "px";
+          canvas.style.height = (windowWidth / gameRatio) + "px";
+      } else {
+          canvas.style.width = (windowHeight * gameRatio) + "px";
+          canvas.style.height = windowHeight + "px";
+      }
+  }
+} */
